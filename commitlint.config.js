f97f2fc70571a3f900a2351416fdc01c0ca67c65
Tanim-Bhuiyann <116840799+Tanim-Bhuiyann@users.noreply.github.com>
2024-11-28 23:50:50 +0600

    adapter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-"export default { extends: ['@commitlint/config-conventional'] };";
+/* "export default { extends: ['@commitlint/config-conventional'] };"; */
+// commitlint.config.js
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // Add any custom rules if needed, e.g.,
+      // 'scope-enum': [2, 'always', ['core', 'docs', 'ui']]
+    },
+  };
+  
